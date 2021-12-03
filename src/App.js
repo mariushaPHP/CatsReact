96959c19b1450f2fb9947cefeace3a881953644e
mariushaPHP <90642649+mariushaPHP@users.noreply.github.com>
2021-12-03 10:41:37 +0200

    pirmas

---
+++ src/App.js
@@ -0,0 +1,34 @@
+//import React from "react";
+import './App.css';
+import Header from "./components/Header";
+import Cats from "./components/Cats";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import {Container} from "react-bootstrap";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+
+const App = () => {
+    return(
+            <Container>
+                <Router>
+
+                    <Header />
+                    <Container style={{margin:"80px"}}>
+                        <Routes>
+                            <Route path="/" element={<Cats/>}/>
+                            <Route path="/about" element={<About/>}/>
+                        </Routes>
+                    </Container>
+
+
+                </Router>
+
+                <Footer />
+            </Container>
+        )
+
+}
+
+export default App;
